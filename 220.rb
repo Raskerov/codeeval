fc7a1de96ed18b6eb265a9f3977b814b7214df83
Raskerov <nutscracker1610@gmail.com>
2016-07-14 09:59:49 +0200

    220 Solved

--- 220.rb
+++ 220.rb
@@ -1,7 +1,6 @@
-=begin
 def input_line(&block)
   input_file.each_line do |line|
-    puts block.call(line.chomp)
+    block.call(line.chomp)
   end
 end
 
@@ -12,20 +11,31 @@ end
 def file_path
   ARGV[0]
 end
-=end
 
-scores = Hash.new
-scores = {
-  Vampires: 1,
-  Zombies: 1,
-  Witches: 1,
-  Houses: 1
-}
+input_line do |line|
+  collected_treats = 0
+  kids = 0
+  scores = Hash.new
+  array = line.split(",")
+  array.each do |e|
+    key = e.split(":")
+    scores[key[0]] = key[1]
+  end
 
-treats = 0
-
-scores.each do |k, v|
-  treats = treats + v.to_i
+      scores.each do |key, value|
+       if key == "Vampires"
+         collected_treats += value.to_i * 3
+         kids += value.to_i
+       elsif key == " Zombies"
+         collected_treats += value.to_i * 4
+         kids += value.to_i
+       elsif key == " Witches"
+         collected_treats += value.to_i * 5
+         kids += value.to_i
+       end
+      end
+      collected_treats = collected_treats * scores[" Houses"].to_i
+      treats_per_kid = collected_treats / kids
+      treats_per_kid.floor
+      puts treats_per_kid
 end
-
-puts treats

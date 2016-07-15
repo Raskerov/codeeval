5b351cc15223d8de0c8cdde1da13424b9e2e952a
Raskerov <nutscracker1610@gmail.com>
2016-07-15 11:30:04 +0200

    220 Code refactored

--- 220.rb
+++ 220.rb
@@ -1,41 +1,76 @@
-def input_line(&block)
-  input_file.each_line do |line|
-    block.call(line.chomp)
-  end
-end
+class Task
+  attr_reader :line
 
-def input_file
-  File.open(file_path, 'r')
-end
-
-def file_path
-  ARGV[0]
-end
-
-input_line do |line|
-  collected_treats = 0
-  kids = 0
-  scores = Hash.new
-  array = line.split(",")
-  array.each do |e|
-    key = e.split(":")
-    scores[key[0]] = key[1]
+  def initialize(line)
+    @line = line
   end
 
-      scores.each do |key, value|
-       if key == "Vampires"
-         collected_treats += value.to_i * 3
-         kids += value.to_i
-       elsif key == " Zombies"
-         collected_treats += value.to_i * 4
-         kids += value.to_i
-       elsif key == " Witches"
-         collected_treats += value.to_i * 5
-         kids += value.to_i
-       end
+  def to_s
+    output.to_s
+  end
+
+  private
+
+  def output
+    counting_kids_and_treats
+    counting_treats_per_kid
+  end
+
+  def counting_treats_per_kid
+    @collected_treats *= @scores[' Houses'].to_i
+    treats_per_kid = @collected_treats / @kids
+    treats_per_kid.floor
+    treats_per_kid
+  end
+
+  def counting_kids_and_treats
+    variables
+    @scores.each do |key, value|
+      if key == 'Vampires'
+        @collected_treats += value.to_i * 3
+        @kids += value.to_i
+      elsif key == ' Zombies'
+        @collected_treats += value.to_i * 4
+        @kids += value.to_i
+      elsif key == ' Witches'
+        @collected_treats += value.to_i * 5
+        @kids += value.to_i
       end
-      collected_treats = collected_treats * scores[" Houses"].to_i
-      treats_per_kid = collected_treats / kids
-      treats_per_kid.floor
-      puts treats_per_kid
+    end
+  end
+
+  def variables
+    @collected_treats = 0
+    @kids = 0
+    defining_scores_as_hash
+  end
+
+  def defining_scores_as_hash
+    @scores = {}
+    array = line.split(',')
+    array.each do |e|
+      key = e.split(':')
+      @scores[key[0]] = key[1]
+    end
+  end
+end
+
+class Reader
+  def self.input_line(&block)
+    input_file.each_line do |line|
+      block.call(line.chomp)
+    end
+  end
+
+  def self.input_file
+    File.open(file_path, 'r')
+  end
+
+  def self.file_path
+    ARGV[0]
+  end
+end
+
+Reader.input_line do |line|
+  puts Task.new(line)
 end
